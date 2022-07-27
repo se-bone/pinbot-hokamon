e2df970a617137dd75f97cb9eec1ca6ea7cee348
se-bone <se.bone.biz@gmail.com>
2022-07-27 22:11:17 +0900

    メッセージが取得できていない問題を修正

--- pinbot_hoc/__main__.py
+++ pinbot_hoc/__main__.py
@@ -78,10 +78,17 @@ async def get_message_from_payload(payload: RawReactionActionEvent) -> Optional[
         Optional[Message]: メッセージ
     """
     channel = get_text_channel_from_payload(payload)
-    if channel is None:
+    thread = get_thread_from_payload(payload)
+    if not (channel or thread):
         return None
 
-    partial_message = channel.get_partial_message(payload.message_id)
+    if channel:
+        partial_message = channel.get_partial_message(payload.message_id)
+    elif thread:
+        partial_message = thread.get_partial_message(payload.message_id)
+    else:
+        logger.error('Channel or thread was not found.')
+
     message = await partial_message.fetch()
 
     return message
@@ -124,6 +131,7 @@ async def on_raw_reaction_add(payload: RawReactionActionEvent):
     message = await get_message_from_payload(payload)
 
     if not (message and (channel or thread)):
+        logger.error('No message, channel, or thread.')
         return
 
     guild = get_guild_from_payload(payload)
