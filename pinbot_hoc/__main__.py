17a75305187c4ce14007fda29f5a1f3e9f56b36f
se-bone <102789224+se-bone@users.noreply.github.com>
2022-07-27 22:04:32 +0900

    add log

--- pinbot_hoc/__main__.py
+++ pinbot_hoc/__main__.py
@@ -124,17 +124,12 @@ async def on_raw_reaction_add(payload: RawReactionActionEvent):
     message = await get_message_from_payload(payload)
 
     if not (message and (channel or thread)):
+        logger.error('No message, channel, or thread.')
         return
 
     guild = get_guild_from_payload(payload)
     if not guild:
         return
-
-    for t in guild.threads:
-        msg = t.get_partial_message(message.id)
-        logger.info(message.id)
-        if msg:
-            logger.info(f'{t.id}, {t.message_count}, {msg.id}')
 
     # :thumbsdown: が3つ以上付けられたメッセージはピン留めしない
     thumbsdown_reaction = [r for r in message.reactions if str(r) == '👎']
